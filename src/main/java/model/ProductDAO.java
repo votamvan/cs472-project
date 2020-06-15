b831c23107ec1f9b0f11ac9290cd8e5c8d03783b
Vo Tam Van <vtvan2k1@gmail.com>
2020-06-15 00:43:44 -0500

    remove no-need import

--- src/main/java/model/ProductDAO.java
+++ src/main/java/model/ProductDAO.java
@@ -1,12 +1,6 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.apache.ibatis.session.SqlSession;
 import mapper.ProductMapper;
 import utils.MyBatisUtil;
